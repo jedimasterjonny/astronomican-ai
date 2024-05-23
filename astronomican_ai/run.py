834e840d838da3e0da4cc37d71c6b12a5defed80
jedmiasterjonny <46541898+jedimasterjonny@users.noreply.github.com>
2024-05-23 14:49:43 +0000

    Ground Gemini to Vertex Datastore containing rules

--- astronomican_ai/run.py
+++ astronomican_ai/run.py
@@ -2,20 +2,31 @@ import os
 import sys
 
 import vertexai
-from vertexai.generative_models import GenerativeModel
+from vertexai.generative_models import GenerativeModel, Tool
+from vertexai.preview.generative_models import grounding
 
 try:
     PROJECT = os.environ["PROJECT"]
+    DS = os.environ["DS"]
 except KeyError:
-    sys.exit("No project set")
+    sys.exit("Required variables unset")
 
 
 def gemini_chat():
     vertexai.init(project=PROJECT, location="europe-north1")
     model = GenerativeModel(model_name="gemini-1.0-pro")
 
+    tool = Tool.from_retrieval(
+        grounding.Retrieval(
+            grounding.VertexAISearch(
+                datastore=f"projects/{PROJECT}/locations/eu/collections/default_collection/dataStores/{DS}",
+            ),
+        ),
+    )
+
     response = model.generate_content(
         "What is Warhammer 40,000?",
+        tools=[tool],
     )
 
     print(response.text)
